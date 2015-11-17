dfe8ff8beedeb4f6dbcd9f39f76ec83427d816a0
scorpion1 <shgr8rb@gmail.com>
2015-11-17 17:55:53 +0500

    add login layout for buyer

--- app/src/main/java/com/byteshaft/auction/login/BuyerLogin.java
+++ app/src/main/java/com/byteshaft/auction/login/BuyerLogin.java
@@ -1,4 +1,15 @@
 package com.byteshaft.auction.login;
 
-public class BuyerLogin {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.byteshaft.auction.R;
+
+public class BuyerLogin extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.buyer_login);
+    }
 }
